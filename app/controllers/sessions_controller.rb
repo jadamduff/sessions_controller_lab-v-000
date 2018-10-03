cc4ab23ddffa6271ae46c114e3bf2498a16bdbcc
Adam Duff <j.adam.duff@gmail.com>
2018-10-03 15:12:15 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     session[:name] = params[:name]
-    redirect_to root_path
+    if session[:name] = nil
+      redirect_to login_path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
